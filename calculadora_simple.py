1b9429b5d1be059974dd496add1645fabfa5ca0d
Alex Dominguez <prueba@email.tk>
2025-10-28 17:16:14 +0100

    Autor: Alex Dominguez

---
+++ calculadora_simple.py
@@ -0,0 +1,36 @@
+'''
+calculadora_simple.py
+Autor: Alex Dominguez
+Descripción: calculadora básica en Python
+Fecha: 28/10/2025
+'''
+
+def sumar (a, b):
+    return a+b
+
+def restar (a, b):
+    return a-b
+def multiplicar (a, b):
+    return a*b
+
+def dividir ( a, b):
+    if b == 0:
+        return "Error: división entre 0."
+    else:
+        return a / b
+
+def main ():
+
+    print('Calculadora simple: ')
+    print('===================')
+
+    x = float(input('Introduce el primer número: '))
+    y = float(input('Introduce el segundo número: '))
+
+    print(f'Suma: {sumar(x,y)}')
+    print(f'Resta: {restar(x,y)}')
+    print(f'Multiplicación: {multiplicar(x,y)}')
+    print(f'División: {dividir(x,y)}')
+
+if __name__ == '__main__':
+    main()
